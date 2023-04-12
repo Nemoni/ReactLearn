cdb6bec03dfdb2270e9313fbede1d6ecaf3b1bb9
Nemoni <490202694@qq.com>
2023-04-12 11:23:26 +0800

    类组件 函数组件 受控组件

--- src/index.js
+++ src/index.js
@@ -1,145 +1,104 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
-function Square(props){
-  return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square 
-        value={this.props.squares[i]} 
-        onClick={()=>this.props.onClick(i)}
-      />
-    );
+// 类组件
+class HelloMessage extends React.Component {
+  handleClick() {
+    console.log("点击了类组件中");
   }
-
+  handleAClick(e) {
+    e.preventDefault();
+    console.log("点击了链接标签");
+  }
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <p>hello {this.props.message}</p>
+        <button onClick={() => this.handleClick()}>点击</button>
+        <a href="www.baidu.com" onClick={this.handleAClick}>
+          链接
+        </a>
       </div>
     );
   }
 }
-
-class Game extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      history:[{
-        squares:Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext:true,
-    };
+// 函数组件
+function FuncComponent(p) {
+  function handleClick() {
+    console.log("点击了");
   }
-
-  handleClick(i){
-    const history = this.state.history.slice(0, this.state.stepNumber+1);
-    const current = history[history.length-1];
-    const squares = current.squares.slice();
-    if(calculateWinner(squares) || squares[i]){
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history:history.concat([{squares:squares}]),
-      stepNumber: history.length,
-      xIsNext:!this.state.xIsNext,
-    });
+  return (
+    <div>
+      <h1>I'm function Component, received: {p.msg}</h1>
+      <button onClick={handleClick}>点我</button>
+    </div>
+  );
+}
+// 箭头函数组件
+const ArrowFuncComponent = (props) => (
+  <h1>Arrow function to create component, received: {props.message}</h1>
+);
+// state setState
+class IncNum extends React.Component {
+  state = {
+    count: 0,
+  };
+  handleClick() {
+    this.setState({ count: this.state.count + 1 });
   }
-
-  jumpTo(step){
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
+  handleClick2 = ()=>{
+    this.setState(prevState=>({count:prevState.count+1})); // state的值有可能变化，使用此处的方法更好
   }
-  
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    const moves = history.map((step, move)=>{
-      const desc = move ?
-      'Go to move #' + move:
-      'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={()=>this.jumpTo(move)}>
-            {desc}
-          </button>
-        </li>
-      )
-    })
-    let status;
-    if(winner){
-      status = "Winner: " + winner;
-    }else{
-      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
-    }
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i)=>this.handleClick(i)}
-          />
+      <div>
+        <p>count:{this.state.count}</p>
+        {/* 在这里解决this指针问题 */}
+        <button onClick={() => this.handleClick()}>+1</button> 
+        {/* 在函数定义处解决this指针问题 */}
+        <button onClick={this.handleClick2}>+1(方式2)</button> 
+      </div>
+    );
+  }
+}
+// 受控组件（让html组件受react控制）
+class BeControled extends React.Component{
+  state = {
+    text: ""
+  };
+  handleInputChange = e=>{
+    this.setState({text:e.target.value});
+    console.log(e.target.value);
+  }
+  render(){
+    return (
+      <div>
+        <input type="text" value={this.state.text} onChange={this.handleInputChange}></input>
+      </div>
+    );
+  }
+}
+// 所有的组件都放到Board上
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* 不可见组件 */}
+        <div style={{ display: "none" }}>
+          <HelloMessage message="baby" />
+          <FuncComponent msg="good" />
+          <ArrowFuncComponent message="arrow" />
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+        {/* 正常显示的组件 */}
+        <div>
+          <IncNum />
+          <BeControled />
         </div>
       </div>
     );
   }
 }
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<Board />);
